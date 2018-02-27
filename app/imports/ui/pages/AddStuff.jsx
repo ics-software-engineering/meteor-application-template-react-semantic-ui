0268ad85eb6361ac8c3fd34d3612e5b6deaf8f8b
Philip Johnson <johnson@hawaii.edu>
2018-02-27 11:20:02 -1000

    checkpoint

--- app/imports/ui/pages/AddStuff.jsx
+++ app/imports/ui/pages/AddStuff.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Stuff, StuffSchema } from '/imports/api/stuff/stuff';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Segment, Header } from 'semantic-ui-react';
 import AutoForm from 'uniforms-semantic/AutoForm';
 import TextField from 'uniforms-semantic/TextField';
 import SelectField from 'uniforms-semantic/SelectField';
@@ -17,39 +17,30 @@ class AddStuff extends React.Component {
     const { name, quantity, condition } = data;
     const username = Meteor.user().username;
     Stuff.insert({ name, quantity, condition, username }, (error) => (error ?
-      Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` }) :
-      Bert.alert({ type: 'success', message: 'Add succeeded' })));
+        Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` }) :
+        Bert.alert({ type: 'success', message: 'Add succeeded' })));
   }
 
   /** Render the form. Use Uniforms: https://github.com/vazco/uniforms */
   render() {
     return (
-      <AutoForm schema={StuffSchema} onSubmit={this.submit} >
-        <Grid container>
-
-          <Grid.Row columns={3}>
-            <Grid.Column>
-              <TextField name='name'/>
-            </Grid.Column>
-            <Grid.Column>
-              <TextField name='quantity'/>
-            </Grid.Column>
-            <Grid.Column>
-              <SelectField name='condition' />
-            </Grid.Column>
-          </Grid.Row>
-
-          <Grid.Row centered>
-            <SubmitField value='Submit'/>
-          </Grid.Row>
-
-          <Grid.Row>
-            <ErrorsField/>
-            <TextField name='username' type='hidden' label={false} value='fakeuser@foo.com'/>
-          </Grid.Row>
-
+        <Grid container centered>
+          <Grid.Column>
+            <Header as="h2" textAlign="center">
+              Add Stuff
+            </Header>
+            <AutoForm schema={StuffSchema} onSubmit={this.submit}>
+              <Segment>
+                <TextField name='name'/>
+                <TextField name='quantity'/>
+                <SelectField name='condition'/>
+                <SubmitField value='Submit'/>
+                <ErrorsField/>
+                <TextField name='username' type='hidden' label={false} value='fakeuser@foo.com'/>
+              </Segment>
+            </AutoForm>
+          </Grid.Column>
         </Grid>
-      </AutoForm>
     );
   }
 }
