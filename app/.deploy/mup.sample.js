484b87259e1d3372f53ec46a362f8df6c3259fb4
Philip Johnson <johnson@hawaii.edu>
2020-10-07 13:43:50 -1000

    add the sample deploy dir

---
+++ app/.deploy/mup.sample.js
@@ -0,0 +1,53 @@
+module.exports = {
+  servers: {
+    one: {
+      host: 'changeme.com',
+      username: 'root',
+      password: 'CHANGEME'
+    }
+  },
+
+  app: {
+    name: 'meteor-application-template-react',
+    path: '../',
+
+    servers: {
+      one: {},
+    },
+
+    buildOptions: {
+      serverOnly: true,
+    },
+
+    env: {
+      ROOT_URL: 'https://changeme.com',
+      MONGO_URL: 'mongodb://mongodb/meteor',
+      MONGO_OPLOG_URL: 'mongodb://mongodb/local',
+    },
+
+    docker: {
+      // abernix/meteord:node-12-base works with Meteor 1.9 - 1.10
+      // If you are using a different version of Meteor,
+      // refer to the docs for the correct image to use.
+      image: 'abernix/meteord:node-12-base',
+    },
+
+    // Show progress bar while uploading bundle to server
+    // You might need to disable it on CI servers
+    enableUploadProgressBar: true
+  },
+
+  mongo: {
+    version: '3.4.1',
+    servers: {
+      one: {}
+    }
+  },
+
+  proxy: {
+    domains: 'changeme.com',
+    ssl: {
+      letsEncryptEmail: 'changeme@hawaii.edu'
+    }
+  }
+};
