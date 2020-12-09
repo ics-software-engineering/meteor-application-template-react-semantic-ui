0e6f199fb9ff2f9230feca89d490d15783b6c179
Philip Johnson <johnson@hawaii.edu>
2020-12-09 12:29:16 -1000

    Add wait call for github actions.

--- app/tests/landing.page.js
+++ app/tests/landing.page.js
@@ -8,7 +8,8 @@ class LandingPage {
 
   /** Asserts that this page is currently displayed. */
   async isDisplayed(testController) {
-    await testController.expect(this.pageSelector.exists).ok();
+    // This is first test to be run. Wait 10 seconds to avoid timeouts with GitHub Actions.
+    await testController.wait(10000).expect(this.pageSelector.exists).ok();
   }
 }
 
