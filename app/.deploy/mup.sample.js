578ac56a92475d0852e606d469bea3e6c842aa86
Philip Johnson <johnson@hawaii.edu>
2021-04-29 09:31:03 -1000

    add cautionary note to mup.js

--- app/.deploy/mup.sample.js
+++ app/.deploy/mup.sample.js
@@ -6,36 +6,21 @@ module.exports = {
       password: 'changeme'
     }
   },
-
   app: {
+    // if you edit the app 'name' field, be sure to run 'mup stop' if the app is already running.
+    // otherwise you will have two apps deployed at once, with unpredictable results.
     name: 'meteor-application-template-react',
     path: '../',
-
-    servers: {
-      one: {},
-    },
-
-    buildOptions: {
-      serverOnly: true,
-    },
-
+    servers: { one: {}, },
+    buildOptions: { serverOnly: true },
     env: {
       ROOT_URL: 'http://111.222.333.444',
       MONGO_URL: 'mongodb://mongodb/meteor',
       MONGO_OPLOG_URL: 'mongodb://mongodb/local',
     },
-
-    docker: {
-      image: 'abernix/meteord:node-12-base',
-    },
-
+    docker: { image: 'abernix/meteord:node-12-base' },
     enableUploadProgressBar: true
   },
-
-  mongo: {
-    version: '3.4.1',
-    servers: {
-      one: {}
-    }
+  mongo: { version: '3.4.1', servers: { one: {} }
   },
 };
